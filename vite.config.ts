3688a90f08f1aac876cec39e4c555c63bce34c13
nichoth <nichoth@nichoth.com>
2025-03-18 12:33:49 -0700

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
+import postcssNesting from 'postcss-nesting'
+import cssnanoPlugin from 'cssnano'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    global: 'globalThis'
+  },
+  publicDir: '_public',
   plugins: [react()],
+  css: {
+    postcss: {
+        plugins: [
+            postcssNesting,
+            cssnanoPlugin
+        ],
+    },
+  },
+  server: {
+    port: 8888,
+    host: true,
+    open: true,
+    proxy: {
+        '/api': {
+            target: 'http://localhost:9999/.netlify/functions',
+            changeOrigin: true,
+            rewrite: path => path.replace(/^\/api/, ''),
+        },
+    },
+  },
+  build: {
+      target: 'esnext',
+      minify: false,
+      outDir: './public',
+      emptyOutDir: true,
+      sourcemap: 'inline'
+  }
 })
