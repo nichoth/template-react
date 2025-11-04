4454ad1afc1fbb5946fbf426d52ee01aec507680
nichoth <nichoth@nichoth.com>
2025-11-04 14:18:36 -0800

    setup netlify

---
+++ netlify/functions/example/example.ts
@@ -0,0 +1,25 @@
+// import type { Context, Config } from '@netlify/functions'
+import type { Context } from '@netlify/functions'
+
+export default async (req:Request, context:Context) => {
+    const { param, splat } = context.params
+    if (req.method !== 'GET') {
+        return new Response(null, { status: 405 })
+    }
+
+    return Response.json({
+        param,
+        splat,
+        hello: 'world'
+    }, {
+        status: 200,
+        headers: {
+            // see https://docs.netlify.com/platform/caching/#durable-directive
+            'Netlify-CDN-Cache-Control': 'public, durable, max-age=60, stale-while-revalidate=120'
+        }
+    })
+}
+
+// export const config:Config = {
+//     path: ['/api/example', '/api/example/:param?', '/api/example/splat/*']
+// }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './app.tsx'
+import '@substrate-system/css-normalize'
+import '@substrate-system/a11y/reduced-motion'
+import './main.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
